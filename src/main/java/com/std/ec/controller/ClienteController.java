8997af0ee84e8b1c42b363bc4fb5d6163473b421
Dayron-z <zbastian.zapata@gmail.com>
2024-04-16 20:36:00 -0500

    Cambio

--- src/main/java/com/std/ec/controller/ClienteController.java
+++ src/main/java/com/std/ec/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.std.ec.controller;
 
 
 
+
 /*        "En el patrón MVC, el controlador recibe las solicitudes del usuario y pasa los datos proporcionados al modelo para su procesamiento."*/
 public class ClienteController {
 }
